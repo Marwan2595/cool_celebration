bdf4328a2e1d9ef563edbbb73507527bee77cd9b
Marwan Sayed <marwan.sayed.soliman95@gmail.com>
2026-06-30 20:40:27 +0300

    base

--- lib/cool_celebration.dart
+++ lib/cool_celebration.dart
@@ -1,1 +1,5 @@
-
+/// A Calculator.
+class Calculator {
+  /// Returns [value] plus 1.
+  int addOne(int value) => value + 1;
+}
--- test/cool_celebration_test.dart
+++ test/cool_celebration_test.dart
@@ -2,4 +2,11 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:cool_celebration/cool_celebration.dart';
 
-void main() {}
+void main() {
+  test('adds one to input values', () {
+    final calculator = Calculator();
+    expect(calculator.addOne(2), 3);
+    expect(calculator.addOne(-7), -6);
+    expect(calculator.addOne(0), 1);
+  });
+}
